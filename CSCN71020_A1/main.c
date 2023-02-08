ff6fb6752ef381a727ee9642341b6d7aed3a4ade
ansharora21 <110608495+ansharora21@users.noreply.github.com>
2023-02-08 11:52:17 -0500

    Adding a new function subtract and a switch case for subtract function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,8 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2: 
+		subtract();
 	}
 
 }
@@ -46,4 +49,14 @@ void add() {
 	scanf_s("%lf", &num3);
 	result = num1 + num2+num3;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
